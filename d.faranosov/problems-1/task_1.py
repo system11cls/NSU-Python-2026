232e21b9666b980a964959cbcd9c0b5b5531e1f0
system11cls <d.faranosov@g.nsu.ru>
2026-02-05 09:39:08 +0700

    task_1_1

---
+++ d.faranosov/problems-1/task_1.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+def get_simple(number: int):
+    number = abs(number)
+    res = []
+    for pretendent in range(2, number//2):
+        if number % pretendent == 0:
+            counter = 0
+            while number % pretendent == 0:
+                counter += 1
+                number /= pretendent
+            res.append([pretendent, counter])
+    if number > 1:
+        res.append([int(number), 1])
+    return res
+
+if len(argv) < 2:
+    print(f'No numbers to work with')
+    exit(1)
+
+
+
+for i, arg in enumerate(argv[1:]):
+    try:
+        number = int(arg)
+        res_list = get_simple(number)
+        print(f'{arg} -> {res_list}\n')
+    except ValueError:
+        print(f'i = {i} number is not an integer or number at all = {arg}')
+        exit(1)
+
